94662a87f0f7eb91593c374813f595730011af87
Chris Lloyd <chris.lloyd.hi@gmail.com>
2020-12-30 18:44:47 +1300

    Test deploy and destroy on infrastructure

--- front-end/infrastructure/bin/infrastructure.ts
+++ front-end/infrastructure/bin/infrastructure.ts
@@ -2,6 +2,14 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
+import { exit } from 'process';
+
+const environment = process.env.ENVIRONMENT ?? undefined;
+if (environment === undefined){
+    console.error("ENVIRONMENT not specified");
+    console.error("Please specify an ENVIRONMENT variable");
+    exit(1);
+}
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack');
+new InfrastructureStack(app, `${environment}-InfrastructureStack`);
