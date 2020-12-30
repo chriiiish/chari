63ed3ae4e2c8f26dad04b3826f6b61b6e9f06716
Chris Lloyd <chris.lloyd.hi@gmail.com>
2020-12-30 19:17:13 +1300

    Change ENVIRONMENT to ENVIRONMENT_CODE

--- front-end/infrastructure/bin/infrastructure.ts
+++ front-end/infrastructure/bin/infrastructure.ts
@@ -4,12 +4,12 @@ import * as cdk from '@aws-cdk/core';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
 import { exit } from 'process';
 
-const environment = process.env.ENVIRONMENT ?? undefined;
-if (environment === undefined){
-    console.error("ENVIRONMENT not specified");
-    console.error("Please specify an ENVIRONMENT variable");
+const environmentCode = process.env.ENVIRONMENT_CODE ?? undefined;
+if (environmentCode === undefined){
+    console.error("ENVIRONMENT_CODE not specified");
+    console.error("Please specify an ENVIRONMENT_CODE variable");
     exit(1);
 }
 
 const app = new cdk.App();
-new InfrastructureStack(app, `${environment}-InfrastructureStack`);
+new InfrastructureStack(app, `${environmentCode}-InfrastructureStack`);
